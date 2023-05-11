134627ca38a959c5635984aa9ebe1754236721db
LindaRobles <l.lindalorena@gmail.com>
2023-05-11 15:52:48 -0600

    src agregado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
 
+
+const valueInput = document.querySelector('#CreditCardNumber')
+const buttonSubmit = document.querySelector('.button')
+const myForm = document.querySelector('#myForm')
+console.log('valueInput-->', valueInput.value)
+console.log('-------->', validator)
+
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  !valueInput.value && alert('Por favor ingrese un número de tarjeta de crédito.')
+
+  const validation = validator.isValid(valueInput.value);
+  const mask =validator.maskify(valueInput.value);
+  
+  if (validation) {
+    alert("La tarjeta de crédito es válida.");
+  } else {
+    alert("La tarjeta de crédito no es válida.");
+  }
+
+  
+document.getElementById("CreditCardNumber").value = mask;
+})
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
-const validator = {
-  // ...
+/* const validator = {
+  isValid: function (creditCardNumber) {
+    // Algoritmo de Luhn para validar la tarjeta va en validator
+    
+  }, 
+
+
 };
+
+export default validator; */
+
+const validator = {
+  isValid: (creditCardNumber) => {
+    let cardArr = creditCardNumber.split("").reverse();
+    let sum = 0;
+    for (let i = 0; i < cardArr.length; i++) {
+      let digit = parseInt(cardArr[i]);
+      if (i % 2 !== 0) { // multiplica por 2 los dígitos en posiciones impares
+        digit *= 2;
+        if (digit > 9) {
+          digit = digit - 9; // suma los dígitos del resultado si es mayor a 9
+        }
+      }
+      sum += digit;
+    }
+    return sum % 10 === 0; // devuelve true si la suma es divisible por 10
+  },
+
+
+maskify(creditCardNumber) {
+  return creditCardNumber.slice(0,-4).replace(/./g, "#") + creditCardNumber.slice(-4);
+}
+};
+
+
 
 export default validator;
